7b8b054f619d6c6f68b8b11d83f205353df9a673
junara <jun5araki@gmail.com>
2022-07-18 21:49:24 +0900

    Add rspec

---
+++ spec/js_dependency/extract_import_path_spec.rb
@@ -0,0 +1,87 @@
+# frozen_string_literal: true
+
+require "rspec"
+
+RSpec.describe JsDependency::ExtractImportPath do
+  describe "self.call" do
+    context "when double quote" do
+      where(:str, :expected) do
+        [
+          ['import defaultExport from "module-name";', ["module-name"]],
+          ['import * as name from "module-name";', ["module-name"]],
+          ['import { export1 } from "module-name";', ["module-name"]],
+          ['import { export1 as alias1 } from "module-name";', ["module-name"]],
+          ['import { export1 , export2 } from "module-name";', ["module-name"]],
+          ['import { foo , bar } from "module-name/path/to/specific/un-exported/file";',
+           ["module-name/path/to/specific/un-exported/file"]],
+          ['import { export1 , export2 as alias2 , [...] } from "module-name";', ["module-name"]],
+          ['import defaultExport, { export1 [ , [...] ] } from "module-name";', ["module-name"]],
+          ['import defaultExport, * as name from "module-name";', ["module-name"]],
+          ['import "module-name";', ["module-name"]],
+          ['var promise = import("module-name");', ["module-name"]]
+        ]
+      end
+
+      with_them do
+        it "returns an array of paths" do
+          expect(described_class.call(str)).to eq(expected)
+        end
+      end
+    end
+
+    context "when single quote" do
+      where(:str, :expected) do
+        [
+          ["import defaultExport from 'module-name';", ["module-name"]],
+          ["import * as name from 'module-name';", ["module-name"]],
+          ["import { export1 } from 'module-name';", ["module-name"]],
+          ["import { export1 as alias1 } from 'module-name';", ["module-name"]],
+          ["import { export1 , export2 } from 'module-name';", ["module-name"]],
+          ["import { foo , bar } from 'module-name/path/to/specific/un-exported/file';",
+           ["module-name/path/to/specific/un-exported/file"]],
+          ["import { export1 , export2 as alias2 , [...] } from 'module-name';", ["module-name"]],
+          ["import defaultExport, { export1 [ , [...] ] } from 'module-name';", ["module-name"]],
+          ["import defaultExport, * as name from 'module-name';", ["module-name"]],
+          ["import 'module-name';", ["module-name"]],
+          ["var promise = import('module-name');", ["module-name"]]
+        ]
+      end
+
+      with_them do
+        it "returns an array of paths" do
+          expect(described_class.call(str)).to eq(expected)
+        end
+      end
+    end
+
+    context "with new line" do
+      let(:str) do
+        <<~STR
+          import {
+            foo,
+          } from 'module-name';
+        STR
+      end
+
+      it "returns an array of paths" do
+        expect(described_class.call(str)).to eq(["module-name"])
+      end
+    end
+
+    context "with other code" do
+      let(:str) do
+        <<~STR
+          console.log("fuga");
+          import {
+            foo,
+          } from 'module-name';
+          console.log("hello");
+        STR
+      end
+
+      it "returns an array of paths" do
+        expect(described_class.call(str)).to eq(["module-name"])
+      end
+    end
+  end
+end
