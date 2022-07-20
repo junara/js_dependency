9541a28f3c1c85cd2310b3cb612c84489f3d516e
junara <jun5araki@gmail.com>
2022-07-20 09:27:49 +0900

    Output parents and children cli with line brake.

--- lib/js_dependency.rb
+++ lib/js_dependency.rb
@@ -57,7 +57,7 @@ module JsDependency
       list << parent_path
     end
     output = list.uniq
-    output_pathname&.write(output)
+    output_pathname&.write(output.join("\n"))
     output
   end
 
@@ -75,7 +75,7 @@ module JsDependency
       list << child_path
     end
     output = list.uniq
-    output_pathname&.write(output)
+    output_pathname&.write(output.join("\n"))
     output
   end
 
