8be20849e9e421edcfdd455a6453ab3533371652
junara <jun5araki@gmail.com>
2022-07-26 00:03:36 +0900

    refactor: change option name to target_paths

--- lib/js_dependency/cli.rb
+++ lib/js_dependency/cli.rb
@@ -9,7 +9,7 @@ module JsDependency
 
     desc "export_mermaid", "Output mermaid flowchart string."
     option :src_path, type: :string, aliases: "-s", desc: "Root folder."
-    option :target_path, type: :array, aliases: "-t", desc: "Target file that you want to analyze."
+    option :target_paths, type: :array, aliases: "-t", desc: "Target file that you want to analyze."
     option :output_path, type: :string, aliases: "-o", desc: "Output file path"
     option :child_analyze_level, type: :numeric, aliases: "-c", desc: "Output level of child dependency"
     option :parent_analyze_level, type: :numeric, aliases: "-p", desc: "Output level of parent dependency"
@@ -22,7 +22,7 @@ module JsDependency
       args = YAML.safe_load(pathname.read) if pathname.exist?
 
       src_path = options[:src_path] || args["src_path"]
-      target_paths = options[:target_path] || (args["target_path"].is_a?(String) ? [args["target_path"]] : args["target_path"])
+      target_paths = options[:target_paths] || (args["target_path"].is_a?(String) ? [args["target_path"]] : args["target_path"])
       child_analyze_level = options[:child_analyze_level] || args["child_analyze_level"] || 2
       parent_analyze_level = options[:parent_analyze_level] || args["parent_analyze_level"] || 2
       output_path = options[:output_path] || args["output_path"] || nil
