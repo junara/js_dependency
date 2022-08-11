d24ca777edc875c3f86b82494ead646585e2708f
junara <jun5araki@gmail.com>
2022-08-11 21:18:47 +0900

    fix: symbolize key

--- lib/js_dependency/cli_utils/config.rb
+++ lib/js_dependency/cli_utils/config.rb
@@ -6,10 +6,12 @@ module JsDependency
       attr_reader :src_path, :target_paths, :target_path, :child_analyze_level, :parent_analyze_level, :output_path,
                   :alias_paths, :name_level, :excludes
 
+      # @param [Hash] options
+      # @param [Hash] args
       def initialize(options, args)
         @src_path = options[:src_path] || args[:src_path]
         @target_paths = options[:target_paths] || (args[:target_path].is_a?(String) ? [args[:target_path]] : args[:target_path])
-        @target_path = options[:target_path] || args["target_path"]
+        @target_path = options[:target_path] || args[:target_path]
         @child_analyze_level = options[:child_analyze_level] || args[:child_analyze_level] || 2
         @parent_analyze_level = options[:parent_analyze_level] || args[:parent_analyze_level] || 2
         @output_path = options[:output_path] || args[:output_path] || nil
