42eb5cb976fda006676c5891e5b3f664587b1552
junara <jun5araki@gmail.com>
2022-08-13 12:04:31 +0900

    Add path option to Yaml

--- lib/js_dependency/cli_utils/yaml.rb
+++ lib/js_dependency/cli_utils/yaml.rb
@@ -3,21 +3,41 @@
 module JsDependency
   module CliUtils
     class Yaml
-      attr_reader :args,
-                  :dir_path
+      attr_reader :dir_path,
+                  :path
 
       # @param [String, nil] dir_path
-      def initialize(dir_path: nil)
-        @dir_path = dir_path || Dir.pwd
-        pathname = config_pathname
-        @args = if pathname.nil?
-                  {}
-                else
-                  symbolize_keys(YAML.safe_load(pathname.read))
-                end
+      # @param [String, nil] path
+      def initialize(dir_path: nil, path: nil)
+        @dir_path = present?(dir_path) ? dir_path : Dir.pwd
+        @path = path
+      end
+
+      # @return [Hash]
+      def args
+        pathname = if present?(@path)
+                     Pathname.new(@path)
+                   else
+                     config_pathname
+                   end
+
+        if pathname.nil?
+          {}
+        else
+          symbolize_keys(YAML.safe_load(pathname.read))
+        end
       end
 
       private
+
+      # @param [String] str
+      # @return [TrueClass, FalseClass]
+      def present?(str)
+        return false if str.nil?
+        return false if str.empty?
+
+        true
+      end
 
       # @return [Pathname, nil]
       def config_pathname
