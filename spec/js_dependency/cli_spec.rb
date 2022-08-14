7f58df48195deac58854c7a62922593a35e9d28f
junara <jun5araki@gmail.com>
2022-08-14 16:54:53 +0900

    add orphan cli test

--- spec/js_dependency/cli_spec.rb
+++ spec/js_dependency/cli_spec.rb
@@ -13,4 +13,50 @@ RSpec.describe JsDependency::Cli do
       expect(output).to include(JsDependency::VERSION)
     end
   end
+
+  describe "#orphan" do
+    context "when src_path is not exist" do
+      let(:options) do
+        {}
+      end
+
+      it "parents command is raised" do
+        expect do
+          described_class.new.invoke(:orphan, [], options)
+        end.to raise_error
+      end
+    end
+
+    context "when src_path is exist" do
+      let(:options) do
+        {
+          src_path: "spec/fixtures/index_creator/self_call/src",
+          alias_paths: {}
+        }
+      end
+
+      it "parents command is not raised" do
+        expect do
+          described_class.new.invoke(:orphan, [], options)
+        end.not_to raise_error
+      end
+    end
+
+    context "when cli command" do
+      let(:expected_output) do
+        <<~OUTPUT
+          components/sub/Exclude.vue
+          pages/app.js
+          utils/calculation.js
+        OUTPUT
+      end
+
+      it "return orphan list" do
+        output = capture(:stdout) do
+          described_class.start(%w[orphan -s spec/fixtures/index_creator/self_call/src -a @:pages])
+        end
+        expect(output).to eq(expected_output)
+      end
+    end
+  end
 end
